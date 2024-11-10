ec374e1d088e53a6f87ad6546971235b919d5c49
Ishan Ramrakhiani <himanalot@gmail.com>
2024-11-10 18:03:52 -0500

    changed secret access

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
 # GPT-4o Mini API credentials and endpoint (replace with your actual API key)
 #h
 GPT4_MINI_API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
-GPT4_MINI_API_KEY = st.secrets["GPT4_MINI_API_KEY"]
-client_id = st.secrets["SPOTIPY_CLIENT_ID"]
-client_secret = st.secrets["SPOTIPY_CLIENT_SECRET"]
+GPT4_MINI_API_KEY = "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"
+client_id = "fe6a043531214ac1abf575878699f986"
+client_secret = "3ca2ef78541a47109b884f42a90881db"
 # Authenticate with Spotify
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=client_id,
