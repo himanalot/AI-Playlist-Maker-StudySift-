25beb2c272c79088271cb7e3850604a353ad437d
Ishan Ramrakhiani <himanalot@gmail.com>
2024-11-10 20:15:09 -0500

    changed api stuff to dummy

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ import threading
 SPOTIPY_REDIRECT_URI = 'http://localhost:8502'  # Or your deployed app URL
 
 GPT4_MINI_API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
-GPT4_MINI_API_KEY = "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"
+GPT4_MINI_API_KEY = "hihi"
 
-client_id = "fe6a043531214ac1abf575878699f986"
-client_secret = "3ca2ef78541a47109b884f42a90881db"
+client_id = "hihihi"
+client_secret = "hihihihi"
 
 # --------------------------- Custom Cache Handler --------------------------- #
 
